3b16220914870e6faa64083c61c9bc914a5ac80a
Lim Joe Quan <joequanlim@gmail.com>
2025-12-24 21:14:28 +0800

    Add keyboard support
    Progress update:
     - No decimal point limit yet
     - Presence of continued operation issues

--- javascript.js
+++ javascript.js
@@ -34,8 +34,68 @@ function operate(num1, operator, num2) {
     return answer
 }
 
-function updateDisplay(num) {
-    display.textContent = num
+function updateDisplay(imput) {
+    display.textContent = imput
+}
+
+function backspace() {
+    if (operator === "") {
+            result = ""
+            num1 = num1.slice(0, -1)
+            updateDisplay(num1)
+            number1 = Number(num1);
+        } else if (operator !== "") {
+            result = ""
+            num2 = num2.slice(0, -1)
+            updateDisplay(num2)
+            number2 = Number(num2);            
+        }
+}
+
+function clear() {
+    num1 = ""
+    num2 = ""
+    operator = ""
+    number1 = ""
+    number2 = ""
+    result = ""
+    updateDisplay(0)
+}
+
+function equals(number1, operator, number2) {
+    if (num2 !== "") {
+        num1 = ""
+        num2 = ""
+        result = operate(number1, operator, number2);
+        result = Number(result.toFixed(4))
+        operator = "";
+        number2 = "";
+        number1 = "";
+        updateDisplay(result);
+    }
+}
+
+function getOperator(imputOperator) {
+        if (result === "") {
+            operator = imputOperator;
+        } else {
+            number1 = result
+            operator = imputOperator;
+        }
+}
+
+function getNumber(imputNum) {
+    if (operator === "" && num1.length < 12) {
+        result = ""
+        num1 = num1 + imputNum
+        updateDisplay(num1)
+        number1 = Number(num1);
+        } else if (operator !== "" && num2.length < 12) {
+        result = ""
+        num2 = num2 + imputNum
+        updateDisplay(num2)
+        number2 = Number(num2)
+        }
 }
 
 
@@ -56,57 +116,48 @@ let display = document.querySelector('.result')
 let btns = document.querySelector(".buttons")
 btns.addEventListener('click', (event) => {
     let targetBtn = event.target
+    targetBtn.blur()
     if ((targetBtn.classList.contains("number") || targetBtn.classList.contains("decimal"))) {
-        if (operator === "" && num1.length < 12) {
-            result = ""
-            num1 = num1 + targetBtn.textContent
-            updateDisplay(num1)
-            number1 = Number(num1);
-        } else if (operator !== "" && num2.length < 12) {
-            result = ""
-            num2 = num2 + targetBtn.textContent
-            updateDisplay(num2)
-            number2 = Number(num2)
-        }
+        getNumber(targetBtn.textContent)
     } else if (targetBtn.classList.contains("operator")) {
-        if (result === "") {
-            operator = targetBtn.textContent;
-            console.log(number1)
-        } else {
-            number1 = result
-            operator = targetBtn.textContent;
-            console.log(number1)
-        }
+        getOperator(targetBtn.textContent)
     } else if (targetBtn.classList.contains("equals") && num2 !== "") {
-        num1 = ""
-        num2 = ""
-        result = operate(number1, operator, number2);
-        result = Number(result.toFixed(4))
-        operator = "";
-        number2 = "";
-        number1 = "";
-        updateDisplay(result);
-        console.log(result)
+        equals(number1, operator, number2)
     } else if (targetBtn.classList.contains("clear")) {
-        num1 = ""
-        num2 = ""
-        operator = ""
-        number1 = ""
-        number2 = ""
-        result = ""
-        updateDisplay(0)
+        clear()
     } else if (targetBtn.classList.contains("delete")) {
-        if (operator === "") {
-            result = ""
-            num1 = num1.slice(0, -1)
-            updateDisplay(num1)
-            number1 = Number(num1);
-        } else if (operator !== "") {
-            result = ""
-            num2 = num2.slice(0, -1)
-            updateDisplay(num2)
-            number2 = Number(num2);            
-        }
+        backspace()
     }
 })
 
+//Keyboard key handler
+window.addEventListener('keydown', (event) => {
+    let targetKey = event.key
+    event.preventDefault()
+    if ((targetKey >= 0 && targetKey <=9) || targetKey === ".") {
+       getNumber(targetKey)
+    } else if (targetKey === "+" || targetKey === "-" || targetKey === "*" || targetKey === "/") {
+        let imputOperator;
+        switch(targetKey) {
+            case "+":
+                imputOperator = "+"
+                break;
+            case "-":
+                imputOperator = "-"
+                break;
+            case "*":               //For multiply, keyboard symbol is different from symbol used for operator!
+                imputOperator = "×"
+                break;
+            case "/":               //For divide, keyboard symbol is different from symbol used for operator!
+                imputOperator = "÷"
+                break;
+        }
+        getOperator(imputOperator)
+    } else if (targetKey === "=" || targetKey === "Enter") {
+        equals(number1, operator, number2)
+    } else if (targetKey === "Escape") {
+        clear()
+    } else if (targetKey === "Backspace"|| targetKey === "Delete") {
+        backspace()
+    }
+})
