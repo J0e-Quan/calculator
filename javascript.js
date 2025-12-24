1097815e60bb5b97c51c50580295de1b58b659f7
Lim Joe Quan <joequanlim@gmail.com>
2025-12-24 15:31:20 +0800

    Implement button imput (except del)
    - Clear button works
    - Handling of variables is fine for chain operations like 4 + 6 / 5 but
    is flawed for others, will fix in the next commit

--- javascript.js
+++ javascript.js
@@ -24,14 +24,66 @@ function operate(num1, operator, num2) {
         case "-":
             result = subtract(num1, num2);
             break;
-        case "*":
+        case "×":
             result = multiply(num1, num2);
             break;
-        case "/":
+        case "÷":
             result = divide(num1, num2);
             break;
     }
     return result
 }
 
-console.log(operate(1, "+", 2))
+function updateDisplay(num) {
+    display.textContent = num
+}
+
+
+
+
+
+
+
+let num1 = ""
+let num2 = ""
+let number1 = ""
+let number2 = ""
+let operator = ""
+let display = document.querySelector('.result')
+
+//Button imput handler
+let btns = document.querySelector(".buttons")
+btns.addEventListener('click', (event) => {
+    let targetBtn = event.target
+    if (targetBtn.classList.contains("number") || targetBtn.classList.contains("decimal")) {
+        if (number1 === "") {
+            num1 = num1 + targetBtn.textContent
+            updateDisplay(num1)
+            console.log("Num1: "+num1)
+        } else if (number1 !== "") {
+            num2 = num2 + targetBtn.textContent
+            updateDisplay(num2)
+            console.log("Num2: "+num2)
+        }
+    } else if (targetBtn.classList.contains("operator")) {
+        operator = targetBtn.textContent;
+        number1 = Number(num1);
+        num1 = "";
+    } else if (targetBtn.classList.contains("equals")) {
+        number2 = Number(num2);
+        num2 = "";
+        let result = operate(number1, operator, number2);
+        operator = "";
+        number2 = "";
+        number1 = result
+        num1 = result
+        updateDisplay(number1);
+    } else if (targetBtn.classList.contains("clear")) {
+        num1 = ""
+        num2 = ""
+        operator = ""
+        number1 = ""
+        number2 = ""
+        updateDisplay(0)
+    }
+})
